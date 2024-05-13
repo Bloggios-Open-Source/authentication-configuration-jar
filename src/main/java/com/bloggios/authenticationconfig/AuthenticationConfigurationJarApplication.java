d0cdd8cde83bb0b9ebce81cef3fd2070f9abc244
Rohit <rohitparih@gmail.com>
2024-05-13 12:47:14 +0530

    Project Initialization

---
+++ src/main/java/com/bloggios/authenticationconfig/AuthenticationConfigurationJarApplication.java
@@ -0,0 +1,13 @@
+package com.bloggios.authenticationconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationConfigurationJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationConfigurationJarApplication.class, args);
+    }
+
+}
